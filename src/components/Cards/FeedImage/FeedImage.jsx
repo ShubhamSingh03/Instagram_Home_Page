b22545c36a4c3afd8d4eb57aac48cdf47f9cebd1
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2022-11-26 11:57:58 +0530

    feedImage component added

---
+++ src/components/Cards/FeedImage/FeedImage.jsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useEffect, useState } from "react";
+// cat api-random
+const url = "https://api.thecatapi.com/v1/images/search?limit=1";
+
+// feed images random component
+const FeedImage = () => {
+    const [imageDetails, setImageDetails] = useState();
+    const getImages = useCallback(() => {
+      let imageDetailsList = [];
+      fetch(url)
+        .then((res) => res.json())
+        .then((images) => {
+          // console.log("Cats: ", cats);
+          for (let i = 0; i < images.length; i++) {
+            imageDetailsList.push(images[i].url);
+          }
+          setImageDetails(imageDetailsList);
+        })
+        .catch((error) => {
+          console.log("Error: ", error);
+        });
+    }, [setImageDetails]);
+  
+    useEffect(() => {
+      getImages();
+    }, [getImages]);
+  
+    return (
+      <>
+        {imageDetails ? (
+          imageDetails.map((imageUrl) => <img src={imageUrl} alt="" className="object-cover bg-white object-center w-screen h-96 border-t border-gray-100"/>)
+        ) : (
+          <p className="text-bold flex justify-center">Loading Your Image...</p>
+        )}
+      </>
+    );
+}
+
+export default FeedImage
